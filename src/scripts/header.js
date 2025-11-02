02564104539b4c100e744a9a809be23fe08674cf
Arden Roddie <aroddie@mcad.edu>
2025-11-01 22:31:42 -0700

    Fixed Nav Menu

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,10 +2,10 @@
     let prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
         let currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
+        if (prevScrollpos > currentScrollPos - 1) {
             document.getElementById("navbar").style.top = "0";
         } else {
-            document.getElementById("navbar").style.top = "-50px";
+            document.getElementById("navbar").style.top = "-55 px";
         }
         prevScrollpos = currentScrollPos;
     }
